915afc6a22af4d4d960bb1cc856c921879a5e67c
doingmov <yohei13111@mail.ru>
2025-11-05 20:14:49 +0300

    Reorder import statements and refactor main function

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,62 +1,64 @@
-import ptbot
-
 import os
-
 import time
 
-from pytimeparse import parse
-
 from dotenv import load_dotenv
-
+from pytimeparse import parse
+import ptbot
 
 
 def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='█', zfill='░'):
     iteration = min(total, iteration)
-    percent = "{0:.1f}"
-    percent = percent.format(100 * (iteration / float(total)))
+    percent = "{0:.1f}".format(100 * (iteration / float(total)))
     filled_length = int(length * iteration // total)
     pbar = fill * filled_length + zfill * (length - filled_length)
-    return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)
+    return "{0} |{1}| {2}% {3}".format(prefix, pbar, percent, suffix)
 
 
 def notify_progress(bot, secs_left, chat_id, message_id, total):
-	remained = total - secs_left
-	progress = render_progressbar(total, remained)
-	if secs_left > 0:
-		bot.update_message(chat_id, message_id,
-			"{0}\nОсталось {1} секунд".format(progress, secs_left))
-	else:
-		bot.update_message(chat_id, message_id,
-			"{0}\nВремя вышло!".format(progress))
+    remained = total - secs_left
+    progress = render_progressbar(total, remained)
+    if secs_left > 0:
+        bot.update_message(
+            chat_id,
+            message_id,
+            "{0}\nОсталось {1} секунд".format(progress, secs_left)
+        )
+    else:
+        bot.update_message(
+            chat_id,
+            message_id,
+            "{0}\nВремя вышло!".format(progress)
+        )
 
 
 def timer(chat_id, message, bot):
-	delay = parse(message)
-	if delay is None or delay <= 0:
-		return
-	message_id = bot.send_message(chat_id, 
-		"Запускаю таймер на {0} секунд".format(delay))
+    delay = parse(message)
+    if delay is None or delay <= 0:
+        return
 
-	
-	for sec in range(delay, 0, -1):
-		notify_progress(bot, sec, chat_id, message_id, delay)
-		time.sleep(1)
-	notify_progress(bot, 0, chat_id, message_id, delay)
+    message_id = bot.send_message(
+        chat_id,
+        "Запускаю таймер на {0} секунд".format(delay)
+    )
+
+    for sec in range(delay, 0, -1):
+        notify_progress(bot, sec, chat_id, message_id, delay)
+        time.sleep(1)
+
+    notify_progress(bot, 0, chat_id, message_id, delay)
+
+
+def handle_message(chat_id, message, bot):
+    timer(chat_id, message, bot)
 
 
 def main():
-	load_dotenv()
-	TG_TOKEN = os.getenv("TG_TOKEN")
-	bot = ptbot.Bot(TG_TOKEN)
+    load_dotenv()
+    TG_TOKEN = os.getenv("TG_TOKEN")
+    bot = ptbot.Bot(TG_TOKEN)
+    bot.reply_on_message(lambda chat_id, message: handle_message(chat_id, message, bot))
+    bot.run_bot()
 
 
-	def handle_message(chat_id, message):
-		timer(chat_id, message, bot)
-
-
-	bot.reply_on_message(handle_message)
-	bot.run_bot()
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
